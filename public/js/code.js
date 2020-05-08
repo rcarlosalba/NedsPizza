d815fad5b69db9ec64508123c53bf4a92fa94388
Carlos Alba <hola@carlosalba.net>
2020-05-08 14:20:20 +0300

    renovación de menu

--- public/js/code.js
+++ public/js/code.js
@@ -1,22 +1,21 @@
 // menuResponsive
 let menuOpen = document.querySelector('#menuOpen')
 let menuClose = document.querySelector('#menuClose')
-let mainMenu = document.querySelector('#mainMenu')
 let menuContainer = document.querySelector('#menuContainer')
 
 function showHide(){
-  if (mainMenu.classList.contains('hidden')) {
+  if (menuContainer.classList.contains('hidden')) {
     // mostrar menu
-    mainMenu.classList.remove('hidden')
-    mainMenu.classList.add('block')
+    menuContainer.classList.remove('hidden')
+    menuContainer.classList.add('block')
     // cambiar boton
     menuOpen.classList.add('hidden')
     menuClose.classList.remove('hidden')
     menuClose.classList.add('block')
   }else{
     // mostrar menu
-    mainMenu.classList.remove('block')
-    mainMenu.classList.add('hidden')
+    menuContainer.classList.remove('block')
+    menuContainer.classList.add('hidden')
     // cambiar boton
     menuClose.classList.add('hidden')
     menuOpen.classList.remove('hidden')
